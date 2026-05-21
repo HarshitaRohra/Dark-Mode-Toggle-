09877f40c3e62e81e59daee68b8be50d9cdb9eee
Harshita Rohra <rohraharshita0@gmail.com>
2026-05-22 01:45:11 +0530

    Changing button text

--- main.js
+++ main.js
@@ -2,5 +2,10 @@ const bttn = document.getElementById("toggleBtn");
 
 bttn.addEventListener("click", () => {
     document.body.classList.toggle("dark");
+    if (document.body.classList.contains("dark")) {
+        bttn.textContent = "Toggle Light Mode";
+    } else {
+        bttn.textContent = "Toggle Dark Mode";
+    }   
 });
 
